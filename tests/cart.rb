f98b50ddff0b6e5d1cbf5e2fcaaf6e3cbb59e023
Rafi Khan <rafi.fa.khan@gmail.com>
2014-06-04 01:47:45 +0200

    Added cart and item file

---
+++ tests/cart.rb
@@ -0,0 +1,34 @@
+class Cart
+  # To store the items, we need to create an array. When a cart is
+  # created, the array will start empty. Note that the constructor
+  # takes no variables!
+  def initialize
+    @items = []
+  end
+
+  # Accessors
+  def items
+    return @items
+  end
+  # Shortcut:
+  # attr_reader :items
+
+  def total_price
+    total = 0
+    @items.each do |item|
+      total += item.price
+    end
+    # Shortcut:
+    # total = @items.inject(0) { |sum, item| sum + item.price }
+    return total.round(2)
+  end
+
+  def add(item)
+    @items.push item
+  end
+
+  def remove(item)
+    @items.delete item
+  end
+end
+
