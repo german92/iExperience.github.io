3d34011e10153d83a1c332b7601b6523f9b628ec
Salman Ansari <salman.ansari@gmail.com>
2014-06-16 11:46:11 +0200

    updates for 16B

--- exercises/randomize.rb
+++ exercises/randomize.rb
@@ -19,12 +19,13 @@ Chauvin)
 
 puts "<table class='reveal bordered'>"
 row = 1
+group_size = 4
 names.shuffle.each_with_index do |name, index|
-  if index%3 == 0
+  if index%group_size == 0
     print "\t<tr>\n\t\t<td>#{row}</td>" 
     row += 1
   end
   print "<td>#{name}</td>"
-  print "\n\t</tr>\n" if index%3 == 2
+  print "\n\t</tr>\n" if index%group_size == group_size - 1
 end
 puts "\n</table>"
